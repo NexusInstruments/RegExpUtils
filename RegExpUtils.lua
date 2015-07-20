0e6868dc61f55dc955310f09762daa48e617ac9d
Chronosis <jayreardon@gmail.com>
2015-07-20 03:46:20 -0700

    Fix typo

--- RegExpUtils.lua
+++ RegExpUtils.lua
@@ -1,5 +1,5 @@
 local PackageName, Major, Minor, Patch = "RegExpUtils", 1, 0, 0
-local PkgMajor, PkgMinor = PackageName, tonumber(sting.format("%02d%02d%02d", Major, Minor, Patch))
+local PkgMajor, PkgMinor = PackageName, tonumber(string.format("%02d%02d%02d", Major, Minor, Patch))
 local Pkg = Apollo.GetPackage(PkgMajor)
 if Pkg and (Pkg.nVersion or 0) >= PkgMinor then
   return -- no upgrade needed
@@ -7,7 +7,6 @@ end
 
 -- Set a reference to the actual package or create an empty table
 local RegExp = Pkg and Pkg.tPackage or {}
-
 
 RegExp.cacheSize = 100 -- this is the size of regex cache
 local g_sourceCache_re = {}
